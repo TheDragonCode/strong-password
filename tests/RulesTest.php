4ab8b643afe110a10b2bda0c27545168c844dad8
Andrey Helldar <helldar@ai-rus.com>
2020-12-21 21:25:28 +0300

    Renamed PasswordTest.php to the RulesTest.php

--- tests/RulesTest.php
+++ tests/RulesTest.php
@@ -4,7 +4,7 @@ namespace Tests;
 
 use Illuminate\Support\Facades\Validator;
 
-final class PasswordTest extends TestCase
+final class RulesTest extends TestCase
 {
     public function testFoo()
     {
@@ -14,12 +14,10 @@ final class PasswordTest extends TestCase
 
     public function testLetters()
     {
-        // 1
         $result = Validator::make(['foo' => 'qwerty'], ['foo' => 'psw_letters']);
         $this->assertFalse($result->fails());
         $this->assertCount(0, $result->errors()->all());
 
-        // 2
         $result = Validator::make(['foo' => '123456'], ['foo' => 'psw_letters']);
         $this->assertTrue($result->fails());
         $this->assertCount(1, $result->errors()->all());
@@ -28,18 +26,15 @@ final class PasswordTest extends TestCase
 
     public function testCaseDiff()
     {
-        // 1
         $result = Validator::make(['foo' => 'qweRTY'], ['foo' => 'psw_case_diff']);
         $this->assertFalse($result->fails());
         $this->assertCount(0, $result->errors()->all());
 
-        // 2
         $result = Validator::make(['foo' => 'qwerty'], ['foo' => 'psw_case_diff']);
         $this->assertTrue($result->fails());
         $this->assertCount(1, $result->errors()->all());
         $this->assertSame('The foo must include both upper and lower case letters.', $result->errors()->first());
 
-        // 3
         $result = Validator::make(['foo' => '123456'], ['foo' => 'psw_case_diff']);
         $this->assertTrue($result->fails());
         $this->assertCount(1, $result->errors()->all());
@@ -48,24 +43,20 @@ final class PasswordTest extends TestCase
 
     public function testNumbers()
     {
-        // 1
         $result = Validator::make(['foo' => 'qweRTY'], ['foo' => 'psw_numbers']);
         $this->assertTrue($result->fails());
         $this->assertCount(1, $result->errors()->all());
         $this->assertSame('The foo must include at least one number.', $result->errors()->first());
 
-        // 2
         $result = Validator::make(['foo' => 'qwerty'], ['foo' => 'psw_numbers']);
         $this->assertTrue($result->fails());
         $this->assertCount(1, $result->errors()->all());
         $this->assertSame('The foo must include at least one number.', $result->errors()->first());
 
-        // 3
         $result = Validator::make(['foo' => '123456'], ['foo' => 'psw_numbers']);
         $this->assertFalse($result->fails());
         $this->assertCount(0, $result->errors()->all());
 
-        // 4
         $result = Validator::make(['foo' => 'qwe123rty'], ['foo' => 'psw_numbers']);
         $this->assertFalse($result->fails());
         $this->assertCount(0, $result->errors()->all());
@@ -73,31 +64,26 @@ final class PasswordTest extends TestCase
 
     public function testSymbols()
     {
-        // 1
         $result = Validator::make(['foo' => 'qweRTY'], ['foo' => 'psw_symbols']);
         $this->assertTrue($result->fails());
         $this->assertCount(1, $result->errors()->all());
         $this->assertSame('The foo must include at least one symbol.', $result->errors()->first());
 
-        // 2
         $result = Validator::make(['foo' => 'qwerty'], ['foo' => 'psw_symbols']);
         $this->assertTrue($result->fails());
         $this->assertCount(1, $result->errors()->all());
         $this->assertSame('The foo must include at least one symbol.', $result->errors()->first());
 
-        // 3
         $result = Validator::make(['foo' => '123456'], ['foo' => 'psw_symbols']);
         $this->assertTrue($result->fails());
         $this->assertCount(1, $result->errors()->all());
         $this->assertSame('The foo must include at least one symbol.', $result->errors()->first());
 
-        // 4
         $result = Validator::make(['foo' => 'qwe123rty'], ['foo' => 'psw_symbols']);
         $this->assertTrue($result->fails());
         $this->assertCount(1, $result->errors()->all());
         $this->assertSame('The foo must include at least one symbol.', $result->errors()->first());
 
-        // 5
         $result = Validator::make(['foo' => 'qwe123r#ty'], ['foo' => 'psw_symbols']);
         $this->assertFalse($result->fails());
         $this->assertCount(0, $result->errors()->all());
@@ -105,25 +91,21 @@ final class PasswordTest extends TestCase
 
     public function testMinLength()
     {
-        // 1
         $result = Validator::make(['foo' => 'qwerty'], ['foo' => 'psw_min_length']);
         $this->assertTrue($result->fails());
         $this->assertCount(1, $result->errors()->all());
         $this->assertSame('The foo must be at least ten characters.', $result->errors()->first());
 
-        // 2
         $result = Validator::make(['foo' => 'qwer'], ['foo' => 'psw_min_length']);
         $this->assertTrue($result->fails());
         $this->assertCount(1, $result->errors()->all());
         $this->assertSame('The foo must be at least ten characters.', $result->errors()->first());
 
-        // 3
         $result = Validator::make(['foo' => 'qwerty123'], ['foo' => 'psw_min_length']);
         $this->assertTrue($result->fails());
         $this->assertCount(1, $result->errors()->all());
         $this->assertSame('The foo must be at least ten characters.', $result->errors()->first());
 
-        // 4
         $result = Validator::make(['foo' => 'qwerty1234'], ['foo' => 'psw_min_length']);
         $this->assertFalse($result->fails());
         $this->assertCount(0, $result->errors()->all());
@@ -131,7 +113,6 @@ final class PasswordTest extends TestCase
 
     public function testStrong()
     {
-        // 1
         $result = Validator::make(['foo' => 'qweRTY'], ['foo' => 'psw_strong']);
         $this->assertTrue($result->fails());
         $this->assertCount(1, $result->errors()->all());
@@ -140,7 +121,6 @@ final class PasswordTest extends TestCase
             $result->errors()->first()
         );
 
-        // 2
         $result = Validator::make(['foo' => 'qwerty'], ['foo' => 'psw_strong']);
         $this->assertTrue($result->fails());
         $this->assertCount(1, $result->errors()->all());
@@ -149,7 +129,6 @@ final class PasswordTest extends TestCase
             $result->errors()->first()
         );
 
-        // 3
         $result = Validator::make(['foo' => '123456'], ['foo' => 'psw_strong']);
         $this->assertTrue($result->fails());
         $this->assertCount(1, $result->errors()->all());
@@ -158,7 +137,6 @@ final class PasswordTest extends TestCase
             $result->errors()->first()
         );
 
-        // 4
         $result = Validator::make(['foo' => 'qwe123rty'], ['foo' => 'psw_strong']);
         $this->assertTrue($result->fails());
         $this->assertCount(1, $result->errors()->all());
@@ -167,7 +145,6 @@ final class PasswordTest extends TestCase
             $result->errors()->first()
         );
 
-        // 5
         $result = Validator::make(['foo' => 'qwe123r#ty'], ['foo' => 'psw_strong']);
         $this->assertTrue($result->fails());
         $this->assertCount(1, $result->errors()->all());
@@ -176,7 +153,6 @@ final class PasswordTest extends TestCase
             $result->errors()->first()
         );
 
-        // 6
         $result = Validator::make(['foo' => null], ['foo' => 'psw_strong']);
         $this->assertTrue($result->fails());
         $this->assertCount(1, $result->errors()->all());
@@ -185,7 +161,6 @@ final class PasswordTest extends TestCase
             $result->errors()->first()
         );
 
-        // 7
         $result = Validator::make(['foo' => 'qwe123R#Ty'], ['foo' => 'psw_strong']);
         $this->assertFalse($result->fails());
         $this->assertCount(0, $result->errors()->all());
