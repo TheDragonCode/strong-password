8f9a38e91095d5e5f004072e73f6e4297c476b47
Andrey Helldar <helldar@ai-rus.com>
2020-12-21 19:54:42 +0300

    Optimized import

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,8 +4,9 @@ namespace Tests;
 
 use Helldar\StrongPassword\ServiceProvider;
 use Illuminate\Support\Facades\Config;
+use Orchestra\Testbench\TestCase as BaseTestCase;
 
-abstract class TestCase extends \Orchestra\Testbench\TestCase
+abstract class TestCase extends BaseTestCase
 {
     protected function getPackageAliases($app)
     {
